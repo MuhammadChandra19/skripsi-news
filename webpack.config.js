0a8cf0829e7a9abd0c45d2bdfe96a840d141261c
muhammad chandra zulfikar <muhammadchandrazulfikar@gmail.com>
2023-01-08 20:35:48 +0700

    init

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin")
+
+const publicPath =  process.argv.indexOf('env=production') >= 0 ? 'https://MuhammadChandra19.github.io/randomuser/' : '/'
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath,
+        filename: 'bundle.js',
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.[jt]sx?$/,
+                use: ['babel-loader'],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+          name: "news-container",
+          filename: "remoteEntry.js",
+          remotes: {},
+          exposes: {}
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './build/index.html'),
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    devServer: {
+        static: path.join(__dirname, './src'),
+        port: 3001,
+        hot: 'only',
+        compress: true,
+        open: true,
+    },
+}
